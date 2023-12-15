ee03427e8fb6d548a0bc8ccd94d5a9b9d6a86aae
unknown <enzo-ctes@hotmail.com>
2023-12-15 15:47:01 -0300

    cambios generales listo para grabar

--- webpersonal17/Indumentaria/views.py
+++ webpersonal17/Indumentaria/views.py
@@ -3,13 +3,22 @@ from .models import Indumentaria
 
 # Create your views here.
 def indumentaria(request):
-    indumentarias = Indumentaria.objects.all()
-    return render(request, "Indumentaria/Indumentaria.html", {'indumentarias':indumentarias})
+    indumentariasNuevas = Indumentaria.objects.filter(historico=False)
+    indumentariasHistoricas = Indumentaria.objects.filter(historico=True)
+    return render(request, "Indumentaria/Indumentaria.html", {'indumentariasNuevas':indumentariasNuevas, 'indumentariasHistoricas':indumentariasHistoricas})
 
-def indumentariasNuevas(request):   
-    indumentariasNuevas = Indumentaria.objects.filter(historico=False)   
-    return render(request, "Indumentaria/Indumentaria.html", {'indumentariaNueva': indumentariasNuevas})
+# # Create your views here.
+# def indumentaria(request):
+#     indumentarias = Indumentaria.objects.filter(historico=True)
+#     return render(request, "Indumentaria/Indumentaria.html", {'indumentarias':indumentarias})
 
-def indumentariasHistoricas(request):
-    indumentariasHistoricas = Indumentaria.objects.filter(historico=True)      
-    return render(request, "Indumentaria/Indumentaria.html", {'indumentariasHistoricas': indumentariasHistoricas})
+
+# def indumentariasNuevas(request):   
+#     indumentarias = Indumentaria.objects.all()
+#     return render(request, "Indumentaria/Indumentaria.html", {'indumentarias':indumentarias})
+    # indumentariasNuevas = Indumentaria.objects.filter(historico=False)   
+    # return render(request, "Indumentaria/Indumentaria.html", {'indumentariaNueva': indumentariasNuevas})
+
+# def indumentariasHistoricas(request):
+#     indumentariasHistoricas = Indumentaria.objects.filter(historico=True)      
+#     return render(request, "Indumentaria/Indumentaria.html", {'indumentariasHistoricas': indumentariasHistoricas})
